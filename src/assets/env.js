7bd0a0193c7daa19d7c4ab45bcc198e47b9a4192
ekosutrisno <ekosutrisno801@gmail.com>
2021-06-04 10:32:38 +0700

    update: add optional URL for simplify development workflow

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,5 +1,8 @@
 const IS_DEV = false;
+const IS_PROD_ERA = true;
 
 export const baseURL = IS_DEV
   ? "http://localhost:9099/file"
+  : IS_PROD_ERA
+  ? "https://microservices-development.erajaya.com:9099/file"
   : "https://shielded-hollows-85991.herokuapp.com/file";
--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -8,7 +8,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z" />
             </svg>  
-          </router-link> <span class="font-medium">Exo</span><span class="text-indigo-500 font-bold">Box</span>
+          </router-link> <span class="font-medium">Li</span><span class="text-indigo-500 font-bold">Box</span>
         </h1>
         <div class="text-gray-900">
           <button @click="openModal = !openModal" class="py-2 px-4 font-semibold text-sm bg-white border inline-flex items-center rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
